470f32e816a2785a2562b8f10d74f4fdd8d22351
Stefan Biereigel <stefan@biereigel.de>
2015-02-02 11:47:00 +0100

    conv_coder: random stimulus test bench

--- hdl/conv_coder/conv_coder_tb.vhd
+++ hdl/conv_coder/conv_coder_tb.vhd
@@ -7,12 +7,19 @@ entity conv_coder_tb is
 end conv_coder_tb;
 
 architecture tb of conv_coder_tb is
+	-- interface signals
 	signal clk 	: std_logic := '0';
 	signal rst 	: std_logic := '1';
 	signal clk_en 	: std_logic := '0';
 	signal d 	: std_logic := '0';
 	signal x 	: std_logic;
 	signal y 	: std_logic;
+	-- number of bits to check
+	constant n_bit	: positive := 1000;
+	-- test vector
+	signal rand_vector	: std_logic_vector(0 to n_bit + 6) := (others => '0');
+	signal x_test		: std_logic;
+	signal y_test		: std_logic;
 
 begin
 	dut : entity work.conv_coder
@@ -21,20 +28,50 @@ begin
 	clk <= not clk after 100 ns;
 	rst <= '0' after 500 ns;
 
-	process
+	prepare : process
 		variable seed1, seed2	: positive;
 		variable rand		: real;
-		variable rand_vector	: std_logic_vector(0 downto 0);
+		variable rand_unsigned	: std_logic_vector(0 downto 0);
 	begin
-		wait until falling_edge(rst);
-		for i in 0 to 255 loop
+		for i in 0 to n_bit-1 loop
 			uniform(seed1, seed2, rand);
-			rand_vector := std_logic_vector(to_unsigned(integer(round(rand)),1));
-			wait until rising_edge(clk);
-			clk_en <= '1';
-			d <= rand_vector(0);
+			rand_unsigned := std_logic_vector(to_unsigned(integer(round(rand)),1));
+			rand_vector(6 + i) <= rand_unsigned(0);
 		end loop;
 	wait;
+	end process;
+
+	test : process
+	begin
+		wait until falling_edge(rst);
+		wait until rising_edge(clk);
+		wait until rising_edge(clk);
+		for i in 6 to n_bit + 6 - 1 loop
+			clk_en <= '1';
+			d <= rand_vector(i);
+			x_test <= 	(rand_vector(i) xor
+					rand_vector(i-1) xor
+					rand_vector(i-2) xor
+					rand_vector(i-3) xor
+					rand_vector(i-6));
+
+			y_test <= 	(rand_vector(i) xor
+					rand_vector(i-2) xor
+					rand_vector(i-3) xor
+					rand_vector(i-5) xor
+					rand_vector(i-6));
+
+			wait until rising_edge(clk);
+			wait until falling_edge(clk);
+
+			assert x = x_test
+			report "X output error."
+			severity failure;
+
+			assert y = y_test
+			report "Y output error."
+			severity failure;
+		end loop;
 	end process;
 end tb;
 
